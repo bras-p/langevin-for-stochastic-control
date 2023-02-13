1bb1a9c783f6ffb42a0d47e2b45ce655d10494cb
Bras-P <pierre.bras@sorbonne-universite.fr>
2023-02-13 16:57:35 +0100

    Add setup.py for distribution

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='langevin-stochastic-control',
+    packages=find_packages(),
+    url='https://github.com/Bras-P/langevin-for-stochastic-control',
+    author="Pierre Bras",
+    description='A framework for comparing optimizers on Stochastic control problems, with an implementation of Langevin optimizers in TensorFlow',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "tensorflow>=2.10.0",
+        "pandas>=1.4.0",
+        "numpy>=1.20.0",
+        "matplotlib>=3.1.0",
+        ],
+    include_package_data=True,
+    license='MIT',
+)
